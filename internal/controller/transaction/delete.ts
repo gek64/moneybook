7bf5550861485e75b73c9d1eba39b4faebf0dc59
gek64 <10346089+gek64@users.noreply.github.com>
2024-07-13 01:39:25 +0800

    fix DeleteTransactions fail when only have one id

--- internal/controller/transaction/delete.ts
+++ internal/controller/transaction/delete.ts
@@ -7,7 +7,8 @@ interface IdQuery {
 }
 
 interface IdsQuery {
-    ids: string[]
+    // 传入一个 id 时为 string, 传入多个 id 时为 string[]
+    ids: string | string[]
 }
 
 async function DeleteTransaction(req: express.Request<any, any, any, IdQuery>, res: express.Response, next: express.NextFunction) {
@@ -43,6 +44,9 @@ async function DeleteTransaction(req: express.Request<any, any, any, IdQuery>, r
 async function DeleteTransactions(req: express.Request<any, any, any, IdsQuery>, res: express.Response, next: express.NextFunction) {
     const query = req.query
     const prisma = new PrismaClient(PrismaClientOption)
+
+    // query.ids 为字符串时转换为单元素数组, 为数组时无改变
+    query.ids = [].concat(query.ids)
 
     // 先删除交易-商品关系表中的关系数据
     await prisma.productOnTransaction.deleteMany({
