c926dfd70e1b6dc9e76f360bf5b8a0df338e54d8
gek64 <10346089+gek64@users.noreply.github.com>
2025-07-03 17:18:03 +0800

    update

--- configs/windows/moneybook.bat
+++ configs/windows/moneybook.bat
@@ -4,6 +4,6 @@ chcp 65001
 set base=%~dp0
 set M_ADDR="0.0.0.0"
 set M_PORT="8000"
-set M_DATABASE="mysql://root:root@192.168.1.2:3306/moneybook"
+set M_DATABASE="mysql://root:root@nas.internal:3306/moneybook"
 
 node index.js -addr %M_ADDR% -p %M_PORT% -db %M_DATABASE%
